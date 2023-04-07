53c6e64454b5ee754907bffb71a70d25228226a8
eliasjp <illumine1998@gmail.com>
2023-04-07 15:15:01 -0300

    feat: create pagination GET and Post with relations

--- traits/models.py
+++ traits/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Trait(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     pets = models.ManyToManyField("pets.Pet", related_name="traits")
